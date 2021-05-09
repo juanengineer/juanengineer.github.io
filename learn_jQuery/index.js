116f80d5e3d92e5e851088051b7ba232337c2151
juanengineer <82479357+juanengineer@users.noreply.github.com>
2021-05-09 18:56:11 -0500

    jQuery lesson and retirement calc

--- learn_jQuery/index.js
+++ learn_jQuery/index.js
@@ -59,10 +59,89 @@ $("h1").on("mouseover",
         $("h1").text("wave that mouse!")
     }
 )
-*/
 
-$("h1").on("click",
+
+$("h1").on
+("click",
     function(){
         $("h1").text("click that mouse!")
+    }
+)
+
+//vanilla JS
+
+for (var i=0; i<5; i++){
+    document.querySelectorAll("button")[i].addEventListener
+    ("click",
+        function(){
+            document.querySelector("h1").style.color="purple"
+        }
+    );
+}
+
+
+//jQuery
+$("button").click(
+    function(){
+        $("h1").css("color","purple");
+    }
+)
+*/
+
+$(document).keypress(
+    function(event){
+        console.log(event.key)
+        $("h1").text(event.key)
+    }
+)
+
+$("h1").on("mouseover",
+    function(){
+        $("h1").text("MO")
+    }
+)
+
+/*
+//toggle hide and show
+$("button").click(
+    function(){
+        $("h1").toggle();
+    }
+)
+
+//fade out
+$("button").click(
+    function(){
+        $("h1").fadeToggle();
+    }
+)
+
+
+//slide up
+$("button").click(
+    function(){
+        $("h1").slideToggle();
+    }
+)
+
+
+//custom animation
+$("button").click(
+    function(){
+        $("h1").animate({opacity: 0.5});
+    }
+)
+
+//animate margin change
+//custom animation
+$("button").click(
+    function(){
+        $("h1").animate({margin: "20%"});
+    }
+)
+*/
+$("button").on("click",
+    function(){
+        $("h1").slideUp().slideDown().animate({opacity: "50%"})
     }
 )
